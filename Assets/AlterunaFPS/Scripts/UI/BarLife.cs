22f786d286bf7e4ed17d846a57cfb93d27a1fe7e
AlexRamos_ <124510650+Alex-RamosV@users.noreply.github.com>
2024-07-06 14:02:51 -0600

    Update BlastForFun v1.15

--- Assets/AlterunaFPS/Scripts/UI/BarLife.cs
+++ Assets/AlterunaFPS/Scripts/UI/BarLife.cs
@@ -1,4 +1,5 @@
 using AlterunaFPS;
+using Alteruna;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,35 +7,52 @@ using UnityEngine.UI;
 
 public class BarLife : MonoBehaviour
 {
-    // Start variables
-    Image image;
+    // UI image to display health bar
+    private Image image;
+    // Reference to the current player GameObject
     private GameObject currentPlayer;
-
-    //Use this for Offline Gameplay
-    //[SerializeField]
-    //PlayerController playerController;
+    // Reference to the Health component
+    private Health currentHealth;
 
     void Awake()
     {
+        // Initialize the Image component
         image = GetComponent<Image>();
     }
-    
+
     void Update()
     {
+        // Find the GameObject with the tag "Player"
         currentPlayer = GameObject.FindGameObjectWithTag("Player");
 
-        if(currentPlayer != null )
+        if (currentPlayer != null)
         {
-            Debug.Log("Player fouind: " + currentPlayer.name);
-            //image.fillAmount = currentPlayer.GetComponent<PlayerController>().MaxHealth / 100;
-            image.fillAmount = currentPlayer.GetComponent<Health>().HealthPoints / 100;
+            // Ensure the player has a Health component
+            currentHealth = currentPlayer.GetComponent<Health>();
+            if (currentHealth != null)
+            {
+                // Print the current health points to the console
+                print(currentHealth.HealthPoints);
+
+                // Decrease health points if the Z key is pressed
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    currentHealth.HealthPoints--;
+                }
+
+                // Update the health bar fill amount
+                image.fillAmount = (float)currentHealth.HealthPoints / 100f;
+
+                Debug.Log("Player found: " + currentPlayer.name);
+            }
+            else
+            {
+                Debug.LogError("Health component not found on player!");
+            }
         }
         else
         {
             Debug.LogError("Player not found!");
         }
-
-        //print(playerController.MaxHealth);
-        //image.fillAmount = playerController.MaxHealth / 100;
     }
 }
